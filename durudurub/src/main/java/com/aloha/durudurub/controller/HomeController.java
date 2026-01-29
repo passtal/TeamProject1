31310d34131c678eebec7397d36609af38e558f7
MoaisMoa <mom.moom.mu@gmail.com>
2026-01-29 09:44:30 +0900

    HomeController 수정

--- durudurub/src/main/java/com/aloha/durudurub/controller/HomeController.java
+++ durudurub/src/main/java/com/aloha/durudurub/controller/HomeController.java
@@ -1,12 +1,17 @@
 package com.aloha.durudurub.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 /**
  * 메인 페이지 컨트롤러
  */
 
+
 @Controller
 public class HomeController {
-
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
 }
