3e143419ea9982573692d9dbeddb577c47596102
hello0a <aya70713@gmail.com>
2026-02-02 15:34:04 +0900

    "헤더 반영"

--- durudurub/src/main/java/com/aloha/durudurub/config/SecurityConfig.java
+++ durudurub/src/main/java/com/aloha/durudurub/config/SecurityConfig.java
@@ -1,7 +1,12 @@
 package com.aloha.durudurub.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 /**
  * Spring Security 설정
@@ -10,4 +15,5 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 @EnableWebSecurity
 public class SecurityConfig {
     // TODO: 구현
+
 }
