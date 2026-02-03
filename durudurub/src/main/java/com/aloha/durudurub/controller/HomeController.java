cdc6590dab75d2dd07a774d46051f36fb46c16d9
hello0a <aya70713@gmail.com>
2026-02-03 15:18:48 +0900

    0203 15시 안영아

--- durudurub/src/main/java/com/aloha/durudurub/controller/HomeController.java
+++ durudurub/src/main/java/com/aloha/durudurub/controller/HomeController.java
@@ -32,6 +32,6 @@ public class HomeController {
             e.printStackTrace();
             model.addAttribute("categories", new ArrayList<>());
         }
-        return "index copy";
+        return "index";
     }
 }
