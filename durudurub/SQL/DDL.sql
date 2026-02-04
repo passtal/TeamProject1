93927fe0314b36f61aa3647f4db61449f4e7571d
passtal <orbiyoung@gmail.com>
2026-02-04 18:08:43 +0900

    260204 18:08 최영우

--- durudurub/SQL/DDL.sql
+++ durudurub/SQL/DDL.sql
@@ -369,6 +369,6 @@ CREATE TABLE `club_location` (
     `location` VARCHAR(255) COMMENT '장소',
     `lat` DECIMAL(10, 8) NULL COMMENT '위도',
     `lng` DECIMAL(11, 8) NULL COMMENT '경도',
-    PRIMARY KEY (`no`),
-    FOREIGN KEY (`location`) REFERENCES `clubs`(`location`) ON DELETE CASCADE
+    PRIMARY KEY (`no`)
+    -- FOREIGN KEY (`location`) REFERENCES `clubs`(`location`) ON DELETE CASCADE
 );
