f472addd58ca8cdf1b6512147a14f08b00e7025e
passtal <orbiyoung@gmail.com>
2026-02-04 17:16:49 +0900

    260204 모임 페이지 지도 좌표 장소 댓글 표시 (모임 참여, 즐겨찾기 미구현)

--- durudurub/src/main/java/com/aloha/durudurub/service/ClubService.java
+++ durudurub/src/main/java/com/aloha/durudurub/service/ClubService.java
@@ -2,7 +2,7 @@ package com.aloha.durudurub.service;
 
 import java.util.List;
 
-import org.apache.ibatis.annotations.Param;
+// import org.apache.ibatis.annotations.Param;
 
 import com.aloha.durudurub.dto.Club;
 import com.aloha.durudurub.dto.ClubMember;
