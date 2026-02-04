fad1112e71c4945c43a1f1be1cf87fdbe459b1f3
hyunsookimm <khsoo0512@gmail.com>
2026-02-04 18:09:36 +0900

    20260204

--- durudurub/SQL/DDL.sql
+++ durudurub/SQL/DDL.sql
@@ -25,6 +25,8 @@ DROP TABLE IF EXISTS
     club_likes,
     random_games,
     banners,
+    report_categories,
+    notices,
     club_member_reports,
     club_location
     ;
@@ -369,6 +371,6 @@ CREATE TABLE `club_location` (
     `location` VARCHAR(255) COMMENT '장소',
     `lat` DECIMAL(10, 8) NULL COMMENT '위도',
     `lng` DECIMAL(11, 8) NULL COMMENT '경도',
-    PRIMARY KEY (`no`),
-    FOREIGN KEY (`location`) REFERENCES `clubs`(`location`) ON DELETE CASCADE
+    PRIMARY KEY (`no`)
+    -- FOREIGN KEY (`location`) REFERENCES `clubs`(`location`) ON DELETE CASCADE
 );
